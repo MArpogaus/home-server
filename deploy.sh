625cd0aa5fbd07605cf9cea2119ae5d1bd27692f
Marcel Arpogaus <38564291+MArpogaus@users.noreply.github.com>
2026-09-23 12:05:45 +0000

    feat: pass deploy_dir to the playbook

--- deploy.sh
+++ deploy.sh
@@ -36,7 +36,7 @@ deploy_ansible() {
         --extra-vars "@${SECRETS_DIR}/secrets/vars.yml" \
         --extra-vars "@${SECRETS_DIR}/secrets/vars.${TARGET_NAME}.yml" \
         --extra-vars "ansible_host=${TARGET_HOST} ansible_port=${TARGET_PORT}" \
-        --extra-vars "secrets_dir=${SECRETS_DIR}"
+        --extra-vars "secrets_dir=${SECRETS_DIR} deploy_dir=${SCRIPT_DIR}"
 }
 
 notify() {
